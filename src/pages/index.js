3ed9a8be0ac5ddef20e5baefb06cb1e9159d5437
Graham Barber <grahammbarber@gmail.com>
2020-01-28 23:52:10 -0800

    add category filters

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo, useState } from 'react'
 import { graphql } from 'gatsby'
 import { format } from 'date-fns'
 import styled from '@emotion/styled'
@@ -10,8 +10,9 @@ import GroupBuy from '../components/group-buy'
 const GridContainer = styled.section`
   width: 100%;
   display: grid;
-  grid-template-columns: 1fr max-content;
+  grid-template-columns: 1fr 948px;
   gap: 48px;
+  justify-content: center;
 `
 
 const Grid = styled.div`
@@ -20,76 +21,128 @@ const Grid = styled.div`
   gap: 24px;
 `
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Group Buys" />
-    <GridContainer>
-      <div style={{ position: 'sticky', top: '48px' }}>
-        <h5
-          style={{ margin: 0, marginBottom: '4px', fontVariant: 'small-caps' }}>
-          filter by category
-        </h5>
-        <div
-          style={{
-            width: '100%',
-            padding: '12px',
-            border: '3px solid #333',
-            marginBottom: '4px'
-          }}>
-          Keycaps
+const CategoryToggle = styled.button`
+  appearance: none;
+  width: 100%;
+  font-family: inherit;
+  border: 3px solid #333;
+  outline: none;
+  border-radius: 0;
+  margin-bottom: 12px;
+  background: #fff;
+  color: #333;
+  font-weight: 700;
+  padding: 12px;
+  transform: translate(-4px, -4px);
+  box-shadow: 8px 8px 0 aquamarine;
+  transition: 0.1s ease-in-out;
+
+  &:hover {
+    transform: translate(-2px, -2px);
+    box-shadow: 6px 6px 0 aquamarine;
+  }
+
+  &:active {
+    transform: translate(4px, 4px);
+    box-shadow: none;
+  }
+
+  &.active {
+    color: #fff;
+    background: #333;
+  }
+
+  &.active:hover {
+    box-shadow: 6px 6px 0 aquamarine;
+  }
+
+  &.active:active {
+    translate(4px, 4px);
+    box-shadow: none;
+  }
+`
+
+const categories = [
+  { label: 'Keycaps', value: 'CAPS' },
+  { label: 'Keyboards', value: 'KEYBOARD' },
+  { label: 'Switches', value: 'SWITCH' },
+  { label: 'Miscellaneous', value: 'MISC' }
+]
+
+const IndexPage = ({ data }) => {
+  const [activeCategory, setActiveCategory] = useState('CAPS')
+  const filteredGroupBuys = useMemo(() => {
+    return data.fauna.allGroupBuys.data.filter(
+      groupBuy => groupBuy.category === activeCategory
+    )
+  }, [activeCategory, data.fauna.allGroupBuys.data])
+
+  return (
+    <Layout>
+      <SEO title="Group Buys" />
+      <GridContainer>
+        <div>
+          <div style={{ position: 'sticky', top: '24px' }}>
+            <h5
+              style={{
+                margin: 0,
+                marginBottom: '12px',
+                fontVariant: 'small-caps'
+              }}>
+              category
+            </h5>
+            {categories.map(category => (
+              <CategoryToggle
+                key={category.value}
+                className={category.value === activeCategory ? 'active' : ''}
+                onClick={() => setActiveCategory(category.value)}>
+                {category.label}
+              </CategoryToggle>
+            ))}
+          </div>
         </div>
-        <div
-          style={{
-            width: '100%',
-            padding: '12px',
-            border: '3px solid #333',
-            marginBottom: '4px'
-          }}>
-          Keyboard
-        </div>
-        <div
-          style={{
-            width: '100%',
-            padding: '12px',
-            border: '3px solid #333',
-            marginBottom: '4px'
-          }}>
-          Switches
-        </div>
-        <div
-          style={{
-            width: '100%',
-            padding: '12px',
-            border: '3px solid #333',
-            marginBottom: '4px'
-          }}>
-          Miscellaneous
-        </div>
-      </div>
-      <Grid>
-        {data.fauna.allGroupBuys.data.map(groupBuy => (
-          <GroupBuy
-            key={groupBuy.name}
-            name={groupBuy.name}
-            date={format(
-              new Date(
-                ...groupBuy.end.split('-').map((n, i) => (i === 1 ? n - 1 : n))
-              ),
-              'LLL do, Y'
-            )}
-            images={groupBuy.images}
-          />
-        ))}
-      </Grid>
-    </GridContainer>
-  </Layout>
-)
+        {filteredGroupBuys.length > 0 ? (
+          <Grid>
+            {filteredGroupBuys.map(groupBuy => (
+              <GroupBuy
+                key={groupBuy.name}
+                name={groupBuy.name}
+                links={groupBuy.links}
+                date={format(
+                  new Date(
+                    ...groupBuy.end
+                      .split('-')
+                      .map((n, i) => (i === 1 ? n - 1 : n))
+                  ),
+                  'LLL do, Y'
+                )}
+                images={groupBuy.images}
+              />
+            ))}
+          </Grid>
+        ) : (
+          <div
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}>
+            <h1 style={{ marginTop: 0 }}>No results</h1>
+            <p>Haha, oops</p>
+          </div>
+        )}
+      </GridContainer>
+    </Layout>
+  )
+}
 
 export const query = graphql`
   query GroupBuyQuery {
     fauna {
       allGroupBuys {
         data {
+          category
           name
           end
           images {
