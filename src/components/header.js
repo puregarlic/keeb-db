99c50ec323cd6a63ed4e7d440e55ce1f7ad35129
Graham Barber <grahammbarber@gmail.com>
2020-01-28 23:24:30 -0800

    add submit button to header

--- src/components/header.js
+++ src/components/header.js
@@ -8,38 +8,69 @@ const HeaderLayout = styled.header`
   justify-content: space-between;
   align-items: center;
   padding: 0 64px;
-  min-height: 80px;
+  min-height: 128px;
 `
 
-const Header = ({ siteTitle }) => (
-  <HeaderLayout>
-    <h3 style={{ margin: 0 }}>
-      <Link
-        to="/"
-        style={{
-          color: '#333',
-          textDecoration: `none`
-        }}>
-        {siteTitle}
-      </Link>
-    </h3>
-    <div>
-      <h4 style={{ margin: 0 }}>
+const SubmitButton = styled(Link)`
+  margin-left: 48px;
+  display: block;
+  appearance: none;
+  border: none;
+  outline: none;
+  padding: 12px 24px;
+  font-size: 16px;
+  font-weight: 700;
+  background: #fff;
+  color: #333;
+  text-decoration: none;
+  border: 3px solid #333;
+  box-shadow: 8px 8px 0 aquamarine;
+  transform: translate(-4px, -4px);
+  transition: 0.1s ease-in-out;
+
+  &:hover {
+    transform: translate(-2px, -2px);
+    box-shadow: 6px 6px 0 aquamarine;
+  }
+
+  &:active {
+    transform: translate(4px, 4px);
+    box-shadow: none;
+  }
+`
+
+const Header = ({ siteTitle }) => {
+  return (
+    <HeaderLayout>
+      <h3 style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
             color: '#333',
-            textDecoration: 'none'
-          }}
-          activeStyle={{
-            textDecoration: 'underline'
+            textDecoration: `none`
           }}>
-          Group Buys
+          {siteTitle}
         </Link>
-      </h4>
-    </div>
-  </HeaderLayout>
-)
+      </h3>
+      <div style={{ display: 'flex', alignItems: 'center' }}>
+        <h4 style={{ margin: 0 }}>
+          <Link
+            to="/"
+            style={{
+              color: '#333',
+              textDecoration: 'none'
+            }}
+            activeStyle={{
+              textDecoration: 'underline'
+            }}>
+            Group Buys
+          </Link>
+        </h4>
+        <SubmitButton to="/submit">Submit</SubmitButton>
+      </div>
+    </HeaderLayout>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string
