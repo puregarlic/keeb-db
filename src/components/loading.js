be2665179da0c033045259b2aaf0f5dee6d05d58
Graham Barber <grahammbarber@gmail.com>
2020-02-09 11:26:19 -0800

    add loading indicator component

---
+++ src/components/loading.js
@@ -0,0 +1,54 @@
+import styled from '@emotion/styled'
+import { keyframes } from '@emotion/core'
+
+const rightSquare = keyframes`
+  25% {
+    transform: translate(-12px, 12px);
+  }
+  50% {
+    transform: translate(-12px, -12px);
+  }
+  75% {
+    transform: translate(12px, -12px);
+  }
+`
+
+const leftSquare = keyframes`
+  25% {
+    transform: translate(12px, -12px);
+  }
+  50% {
+    transform: translate(12px, 12px);
+  }
+  75% {
+    transform: translate(-12px, 12px);
+  }
+`
+
+const Loading = styled.div`
+  &::before {
+    position: absolute;
+    content: '';
+    display: block;
+    width: 24px;
+    height: 24px;
+    background: aquamarine;
+    transform-origin: center;
+    transform: translate(12px, 12px);
+    animation: 1s ${rightSquare} ease-in-out infinite;
+  }
+
+  &::after {
+    position: absolute;
+    content: '';
+    display: block;
+    width: 24px;
+    height: 24px;
+    background: #333;
+    transform-origin: center;
+    transform: translate(-12px, -12px);
+    animation: 1s ${leftSquare} ease-in-out infinite;
+  }
+`
+
+export default Loading
