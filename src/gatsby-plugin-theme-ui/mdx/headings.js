cee082c2d31b05b65d68c3a82ada1fabf24d3dcb
Graham Barber <grahammbarber@gmail.com>
2020-02-11 15:09:01 -0800

    tweak typographical scale

--- src/gatsby-plugin-theme-ui/mdx/headings.js
+++ src/gatsby-plugin-theme-ui/mdx/headings.js
@@ -1,6 +1,4 @@
 const headingBase = {
-  mt: 5,
-  mb: 4,
   fontFamily: 'heading',
   fontWeight: 'bold',
   lineHeight: 'heading',
@@ -11,26 +9,34 @@ export default {
   h1: {
     ...headingBase,
     mt: 0,
-    fontSize: 7
+    fontSize: 7,
+    mb: 4
   },
   h2: {
     ...headingBase,
-    fontSize: 6
+    fontSize: 6,
+    mb: 4,
+    mt: 5
   },
   h3: {
     ...headingBase,
-    fontSize: 5
+    fontSize: 5,
+    mb: 3,
+    mt: 5
   },
   h4: {
     ...headingBase,
-    fontSize: 4
+    fontSize: 4,
+    mb: 3
   },
   h5: {
     ...headingBase,
-    fontSize: 3
+    fontSize: 3,
+    mb: 2
   },
   h6: {
     ...headingBase,
-    fontSize: 2
+    fontSize: 2,
+    mb: 2
   }
 }
