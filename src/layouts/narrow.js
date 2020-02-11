78ab2d0007a95dd4efaca9bec0d84379c846c364
Graham Barber <grahammbarber@gmail.com>
2020-02-11 00:12:30 -0800

    remove text justification from narrow layout styles

--- src/layouts/narrow.js
+++ src/layouts/narrow.js
@@ -4,9 +4,7 @@ const Narrow = ({ children }) => (
   <main
     style={{
       maxWidth: '57ch',
-      margin: '0 auto',
-      textAlign: 'justify',
-      textJustify: 'inter-word'
+      margin: '0 auto'
     }}>
     {children}
   </main>
